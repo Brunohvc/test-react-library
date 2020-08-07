a3f0d774ed6ac207dc09142d441dc666fbca894b
Bruno Henrique <brunohvcarvalho@gmail.com>
2020-08-07 18:07:07 -0300

    redirect to main page

--- src/Routes/Routes.js
+++ src/Routes/Routes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import PrivateRouter from './PrivateRouter';
 import BooksList from '../Pages/BooksList';
 import Login from '../Pages/Login';
@@ -9,6 +9,7 @@ const Routes = () => (
         <Switch>
             <Route exact path="/login" component={Login} />
             <PrivateRouter exact path="/" component={BooksList} />
+            <Route path='*' component={() => (<Redirect to={{ pathname: '/' }} />)} />
         </Switch>
     </BrowserRouter>
 )
